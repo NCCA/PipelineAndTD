dda5b363c2bc6116e7c00662f93b6abd5e330beb
jmacey <jmacey@bournemouth.ac.uk>
2022-04-20 09:31:14 +0100

    added new thread demo

--- Lecture8Threading/c++11Threads/thread1.cpp
+++ Lecture8Threading/c++11Threads/thread1.cpp
@@ -1,27 +1,14 @@
-#include <iostream>
 #include <thread>
-#include <cstdlib>
-#include "Logger.h"
-#include <vector>
-#include <fmt/format.h>
-#include <fmt/ostream.h>
+#include <iostream>
 
-void hello()
+void threadFunc()
 {
-	std::thread::id this_id = std::this_thread::get_id();
-	for (int i = 0; i < 10; ++i)
-		Logger::info(fmt::format("{0} hello from thread {1}", this_id, i));
+	std::cout << "ID " << std::this_thread::get_id() << '\n';
 }
 
 int main()
 {
-	auto nThreads = std::thread::hardware_concurrency();
-	std::cout << "num threads " << nThreads << '\n';
-	std::vector<std::thread> pool(nThreads);
-	for (auto &t : pool)
-		t = std::thread(hello);
-	for (auto &t : pool)
-		t.join();
-
-	return EXIT_SUCCESS;
+	std::cout << "Parent " << std::this_thread::get_id() << '\n';
+	std::thread t1(threadFunc);
+	t1.join();
 }
