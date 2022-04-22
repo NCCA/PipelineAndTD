5698ed25a804886e27338f8c05ae12a0a89c7e86
jmacey <jmacey@bournemouth.ac.uk>
2022-04-22 13:54:59 +0100

    added new threading demos

---
+++ Lecture9Threading/lambda/lambda1.cpp
@@ -0,0 +1,12 @@
+#include <thread>
+#include <iostream>
+#include "Logger.h"
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+int main()
+{
+  Logger::warning(fmt::format("Parent id {}", std::this_thread::get_id()));
+  std::thread t1([]()
+                 { Logger::info(fmt::format("In thread {}", std::this_thread::get_id())); });
+}
---
+++ Lecture9Threading/pool/Pool.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <thread>
+#include <iostream>
+#include "Logger.h"
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+#include <numeric>
+#include <random>
+
+struct Pool
+{
+  Pool(size_t _size)
+  {
+    things.resize(_size);
+    std::iota(std::begin(things),std::end(things),0);
+  }
+  int getThing()
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    while(things.empty())
+    {
+        Logger::critical("Waiting Lock");
+        cv.wait(lock);
+    }
+    auto r=std::move(things.back()); // I know it's an int
+    things.pop_back();
+    return r;
+  }
+
+  void returnThing(int _t)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    things.push_back(_t);
+    lock.unlock();
+    Logger::error("calling Notify");
+    cv.notify_one();
+  }
+  std::vector<int> things;
+  std::mutex mutex;
+  std::condition_variable cv;
+};
+
+int main()
+{
+  std::default_random_engine rng;
+  std::uniform_int_distribution<int> sleep_for(500,1550);
+
+  Pool pool(4);
+	auto nThreads = std::thread::hardware_concurrency();
+  Logger::info(fmt::format("Have {} threads",nThreads));
+	std::vector<std::thread> workers(nThreads);
+  for(auto &t : workers)
+  {
+    t=std::thread([&](){
+      while(true)  
+      {
+        // grab thing from pool
+        auto thing=pool.getThing();
+        Logger::warning(fmt::format("Thread {} has thing {}",std::this_thread::get_id(),thing));
+        // simulate work
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_for(rng)));
+        // return when done
+        pool.returnThing(thing);
+      }
+    });
+  }
+  for(auto &t : workers)
+    t.join();
+}
