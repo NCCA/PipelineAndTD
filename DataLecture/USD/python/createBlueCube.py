c1f39de33ed6ca0c633a66bd1f28435d0d787fec
jmacey <jmacey@bournemouth.ac.uk>
2024-03-11 12:26:44 +0000

    added new USD python demos

---
+++ DataLecture/USD/python/createBlueCube.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from pxr import Usd, UsdGeom
+
+# Create a temporary stage in memory
+stage = Usd.Stage.CreateInMemory("Cube.usda")
+
+# Create a transform and add a Cube as mesh data
+xformPrim = UsdGeom.Xform.Define(stage, "/Cube")
+
+# Set a translation
+UsdGeom.XformCommonAPI(xformPrim).SetTranslate((0, 0, 0))
+
+cubePrim = UsdGeom.Cube.Define(stage, "/Cube/MeshData")
+
+# Get the sphere as a generic prim
+cube = stage.GetPrimAtPath("/Cube/MeshData")
+
+# Get the dimensions
+size = cubePrim.GetSizeAttr()
+
+# Access the cube schema to set the color
+colorAttr =cubePrim.GetDisplayColorAttr()
+
+size.Set(2.9)
+
+
+# Make the cube blue
+colorAttr.Set([(0.0, 0.0, 1.0)])
+
+# Print out the stage
+print(stage.GetRootLayer().ExportToString())
+
+# Save the resulting layer
+stage.Export("BlueCube.usda", addSourceFileComment=False)
--- DataLecture/USD/python/createCube.py
+++ DataLecture/USD/python/createCube.py
@@ -5,30 +5,27 @@ from pxr import Usd, UsdGeom
 stage = Usd.Stage.CreateInMemory("Cube.usda")
 
 # Create a transform and add a Cube as mesh data
-xformPrim = UsdGeom.Xform.Define(stage, "/Cube")
-
-# Set a translation
-UsdGeom.XformCommonAPI(xformPrim).SetTranslate((0, 0, 0))
-
-cubePrim = UsdGeom.Cube.Define(stage, "/Cube/MeshData")
-
-# Get the sphere as a generic prim
-cube = stage.GetPrimAtPath("/Cube/MeshData")
-
-# Get the dimensions
-size = cubePrim.GetSizeAttr()
-
-# Access the cube schema to set the color
-colorAttr =cubePrim.GetDisplayColorAttr()
-
-size.Set(2.9)
+xformPrim = UsdGeom.Xform.Define(stage, "/World")
 
 
-# Make the cube blue
-colorAttr.Set([(0.0, 0.0, 1.0)])
+prim = UsdGeom.Mesh.Define(stage, "/World/Cube")
+prim.CreatePointsAttr().Set([(-0.5, -0.5, 0.5), (0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (-0.5, 0.5, -0.5), (0.5, 0.5, -0.5), (-0.5, -0.5, -0.5), (0.5, -0.5, -0.5)])
+prim.CreateFaceVertexCountsAttr().Set([4, 4, 4, 4, 4, 4])
+prim.CreateFaceVertexIndicesAttr().Set([0, 1, 3, 2, 2, 3, 5, 4, 4, 5, 7, 6, 6, 7, 1, 0, 1, 7, 5, 3, 6, 0, 2, 4])
+prim.CreateExtentAttr().Set([(-0.5, -0.5, -0.5), (0.5, 0.5, 0.5)])
+
+# Set the color of the cube
+colorAttr = prim.GetDisplayColorAttr()
+colorAttr.Set([(0.463, 0.725, 0.0)])
+
+# Add transforms to the cube
+UsdGeom.XformCommonAPI(prim).SetRotate((0, 45.0, 0))
+UsdGeom.XformCommonAPI(prim).SetTranslate((1, 0, 0))
+UsdGeom.XformCommonAPI(prim).SetScale((1, 2.0, 1.0))
+
 
 # Print out the stage
 print(stage.GetRootLayer().ExportToString())
 
 # Save the resulting layer
-stage.Export("BlueCube.usda", addSourceFileComment=False)
+stage.Export("cube.usda", addSourceFileComment=False)
