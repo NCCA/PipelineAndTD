5e37310161218ae4da6a4e0833baf7849eae09d9
jmacey <jmacey@bournemouth.ac.uk>
2024-03-14 13:02:18 +0000

    updated primitives demo

--- DataLecture/USD/python/primitives.py
+++ DataLecture/USD/python/primitives.py
@@ -16,14 +16,38 @@ UsdGeom.SetStageUpAxis(stage, UsdGeom.Tokens.y)
 UsdGeom.SetStageMetersPerUnit(stage, 1.0) 
 
 shapeMesh = UsdGeom.Sphere.Define(stage, "/World/Sphere")
-position_prim(shapeMesh.GetPrim(), (1, 0, 1))
+position_prim(shapeMesh.GetPrim(), (2, 0, 0))
 shapeMesh = UsdGeom.Cube.Define(stage, "/World/Cube")
-position_prim(shapeMesh.GetPrim(), (-1, 0, 1))
+position_prim(shapeMesh.GetPrim(), (-2, 0, 2))
 
-# Add transforms to the cube
-# UsdGeom.XformCommonAPI(prim).SetRotate((0, 45.0, 0))
-# UsdGeom.XformCommonAPI(prim).SetTranslate((1, 0, 0))
-# UsdGeom.XformCommonAPI(prim).SetScale((1, 2.0, 1.0))
+shapeMesh = UsdGeom.Cone.Define(stage, "/World/Cone")
+position_prim(shapeMesh.GetPrim(), (2, 0, 2))
+
+shapeMesh = UsdGeom.Capsule.Define(stage, "/World/Capsule")
+position_prim(shapeMesh.GetPrim(), (2, 0, -2))
+
+shapeMesh = UsdGeom.Cylinder.Define(stage, "/World/Cylinder")
+position_prim(shapeMesh.GetPrim(), (-2, 0, -2))
+
+
+# a row of cylinders with different axis
+shapeMesh = UsdGeom.Cylinder.Define(stage, "/World/CylinderXAxis")
+position_prim(shapeMesh.GetPrim(), (-2, 0, -4))
+prim=shapeMesh.GetPrim()
+shapeMesh.CreateAxisAttr("X")
+
+
+shapeMesh = UsdGeom.Cylinder.Define(stage, "/World/CylinderYAxis")
+position_prim(shapeMesh.GetPrim(), (0, 0, -4))
+prim=shapeMesh.GetPrim()
+shapeMesh.CreateAxisAttr("Y")
+
+shapeMesh = UsdGeom.Cylinder.Define(stage, "/World/CylinderZAxis")
+position_prim(shapeMesh.GetPrim(), (2, 0, -4))
+prim=shapeMesh.GetPrim()
+shapeMesh.CreateAxisAttr("Z")
+
+
 
 # Print out the stage
 print(stage.GetRootLayer().ExportToString())
