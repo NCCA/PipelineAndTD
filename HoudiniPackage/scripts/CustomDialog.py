8e058b18cfbc82f933af97f7023d79cd55cb1039
Jon Macey <jmacey@bournemouth.ac.uk>
2025-05-14 11:58:05 +0100

    updated package example

--- HoudiniPackage/scripts/CustomDialog.py
+++ HoudiniPackage/scripts/CustomDialog.py
@@ -7,10 +7,12 @@ class MyDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.setWindowTitle(hou.getenv("CUSTOM_ENV"))
         layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(QtWidgets.QLabel("Hello from Qt!"))
+        layout.addWidget(QtWidgets.QLabel(hou.getenv("CUSTOM_ENV")))
+        self.resize(400, 400)
 
 
 def run_package():
+    
     # Parent to Houdini main window
     dialog = MyDialog(parent=hou.ui.mainQtWindow())
     dialog.show()  # or dialog.exec() for modal
