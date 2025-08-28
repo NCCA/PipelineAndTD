b348ab2adfb7cc559f69a46896a30a3526f6b0a3
Jon Macey <jmacey@bournemouth.ac.uk>
2025-08-28 20:04:35 +0100

    added simple gui testing demo

---
+++ PySideGuiTesting/main.py
@@ -0,0 +1,27 @@
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+import sys
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Demo App")
+
+        self.label = QLabel("Not clicked yet")
+        self.button = QPushButton("Click me")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+        # Connect button click to handler
+        self.button.clicked.connect(self.on_button_click)
+
+    def on_button_click(self):
+        self.label.setText("Button was clicked!")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
--- QtPyStarter/QtPyStarter.py
+++ QtPyStarter/QtPyStarter.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run --active --script
 import sys
-from qtpy import QtCore,QtWidgets
+
+from qtpy import QtWidgets
 
 
+class SimpleDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(SimpleDialog, self).__init__(parent)
+        self.setWindowTitle("Simple Dialog")
+        self.setMinimumWidth(200)
 
-class SimpleDialog(QtWidgets.QDialog) :
-  def __init__(self,parent=None) :
-    super(SimpleDialog,self).__init__(parent)
-    self.setWindowTitle("Simple Dialog")
-    self.setMinimumWidth(200)
-    
 
-if __name__ == "__main__" :
-  app = QtWidgets.QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
 
-  dialog=SimpleDialog()
-  dialog.show()
-  app.exec_()
+    dialog = SimpleDialog()
+    dialog.show()
+    app.exec_()
